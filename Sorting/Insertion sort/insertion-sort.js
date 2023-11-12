edb4c2572aa02a998403323be6089fc547b03b23
natesh <natesh@nateshs-MacBook-Pro.local>
2023-11-12 18:21:37 +0530

    insertion sort js

---
+++ Sorting/Insertion sort/insertion-sort.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let temp = arr[i];
+    let j;
+    for (j = i - 1; j >= 0 && arr[j] > temp; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = temp;
+  }
+  return arr;
+}
+
+testArr = [16, 11, 44, 55, 88, 6];
+
+result = insertionSort(testArr);
+console.log(result);
